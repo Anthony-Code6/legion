c22efcb17d4b87a14d8ed3be6cdc8385e6dbaaf1
Anthony-Code6 <69853540+Anthony-Code6@users.noreply.github.com>
2025-05-12 05:00:47 -0500

    delete reference @angular/youtube-player file config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
-import { YOUTUBE_PLAYER_CONFIG } from '@angular/youtube-player';
-
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), {
-    provide: YOUTUBE_PLAYER_CONFIG, useValue: {
-      disablePlaceholder: true,
-
-    }
-  }]
+  providers: [provideRouter(routes), provideAnimationsAsync()]
 };
